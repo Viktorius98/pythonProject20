c0e31fbd07dea9030574718c648c6d3d3e4a25e9
Viktorius98 <viksin376@yandex.ru>
2022-11-02 21:52:12 +0300

    Add files1

---
+++ tests/test1.py
@@ -0,0 +1,20 @@
+import pytest
+
+from app.calculator import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator()
+
+    def test_adding_succes(self):
+        assert self.calc.adding(1, 1) == 2
+
+    def test_test_adding_unsucces(self):
+        assert self.calc.adding(1, 1) == 3
+
+    def test_zero_division(self):
+        with pytest.raises(ZeroDivisionError):
+            self.calc.division(1, 0)
+
+    def teardowm(self):
+        print('Выполнение метода Teardown')
